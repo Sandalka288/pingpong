37a7f0cd35949b811f84a74f615443e35f165381
Sandalka288 <test@gmail.com>
2025-05-11 17:54:27 +0500

    wip

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import os
 class GameSprate(sprite.Sprite):
     def __init__(self, img, x, y, speed, wight=55, height=55):
         super().__init__()
-        self.img = transform.scale(image.load(img), (wight, height))
+        self.img = transform.scale(
+            image.load(img),
+            (wight, height)
+        )
         self.speed = speed
         self.rect = self.img.get_rect()
         self.rect.x = x
@@ -13,16 +16,18 @@ class GameSprate(sprite.Sprite):
     def reset(self):
         window.blit(self.img, (self.rect.x, self.rect.y))
 
+
 class Player(GameSprate):
-    def update(part='left'):
+    def update(self, part='left'):
         keys_data = {
-            'left':
+            'left': [K_UP, K_DOWN],
+            'right': [K_w, K_s]
         }
-        key = get_pressed():
-        if key[K_UP] and self.rect.y > 5:
+        keys = key.get_pressed()
+        if keys[keys_data[part][0]] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if key[K_DOWN] and self.rect.y < win_h - 80:
-            self.rect.x += self.speed
+        if keys[keys_data[part][1]] and self.rect.y < win_h - 80:
+            self.rect.y += self.speed
 
 
 background_c = (200, 255, 255)
@@ -42,8 +47,11 @@ FPS = 60
 speed_x, speed_y = 3, 3
 path = os.getcwd()
 ball = GameSprate(f"{path}\\test.png", 200, 200, 4, 50, 50)
+rocket1 = Player(f'{path}\\zenit.jpg', 30, 200, 4, 50, 150)
+rocket2 = Player(f'{path}\\zenit.jpg', 530, 200, 4, 50, 150)
 
-rocket = Player('zenit.jpg')
+font.init()
+font = font.Font(None, 35)
 
 while game:
     for e in event.get():
@@ -54,16 +62,28 @@ while game:
         window.fill(background_c)
         ball.rect.x += speed_x
         ball.rect.y += speed_y
+        rocket1.update('left')
+        rocket2.update('right')
+
+        if sprite.collide_rect(rocket1, ball) or sprite.collide_rect(rocket2, ball):
+            speed_x *= -1
+            speed_y *= 1
 
         if ball.rect.y > win_h-50 or ball.rect.y < 0:
             speed_y *= -1
 
-        if ball.rect.x < 0 or ball.rect.x > win_w:
-            speed_y *= -1
-            speed_x *= -1
+        if ball.rect.x < 0:
+            finish = False
+            window.blit(font.render('LEFT LOSE', True, (180, 0, 0)), 200)
+
+        if ball.rect.x > win_w:
+            finish = False
+            window.blit(font.render('RIGHT LOSE', True, (180, 0, 0)), 200)
+            
 
         ball.reset()
-
+        rocket1.reset()
+        rocket2.reset()
 
     display.update()
     clock.tick(FPS)
